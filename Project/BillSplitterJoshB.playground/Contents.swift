33cfd9ecf5698ca74b537f0e95ea0c351ebe17be
jtbarre2 <jtbarre2@illinois.edu>
2016-10-31 23:07:58 -0500

    Worked on Project

--- Project/BillSplitterJoshB.playground/Contents.swift
+++ Project/BillSplitterJoshB.playground/Contents.swift
@@ -1,10 +1,87 @@
-/*Bill Splitter app:
- This app lets you input a bill amount for rent, utilities, etc. and then the number of people to split between which can have different percentages applied to it and the app will return what each person should pay.
- There also is another area for splitting a restaurant bill which includes a tip calculator so you can input the tip percentage you want to give and that will be applied to the split amounts.*/
+/*Bill Splitter App
+This app is for splitting a bill between multiple people and helping with tip calculation
+as well as splitting it out amongst the group.
+The first portion will be able to take in a restaurant bill total, give back a total with a specified
+tip precentage added in and then from there you can either split it evenly or specify the percentages to dole out to each person.
+The next function will be for splitting rent and will include things like cable, water, electric or w/e other utilities, maybe add in a way to vary the number of different factors.
+Could also add in just a random function for paying someone back such as gas for a car trip etc.*/
 
-import UIKit
+/*
+    _______________________________________
 
-var nsplits = 4
+    |  Restaurant|   Rent   |  Other       |
+    |  ____________________________________|
+    |                                      |
+    |                                      |
+    |   Bill, pre-tax :  ___________       |
+    |                                      |
+    |   Bill, after-tax: ___________       |
+    |                                      |
+    |                                      |
+    |    Tip %: ______                     |
+    |                                      |
+    |                                      |
+    |    Number of People to Divide: ____  |
+    |       Evenly? ___                    |
+    |                                      |
+    |                                      |
+    |                                      |
+    |                                      |
+    |                                      |
+    |                                      |
+    |                                      |
+    |                                      |
+    |                                      |
+    |                                      |
+    |                                      |
+    |                                      |
+    |                                      |
+    _______________________________________
 
-var percents:[Float]
-for var i = 0; i == nsplits; i+=1
+
+
+
+*/
+
+//Set up any necessary variables
+var pretaxtotal: Double
+var posttaxtotal: Double
+var tippercent: Double
+var finaltotal: Double
+var numsplit: Int
+var splitpercent: [Double] = []
+var finalsplit: [Double] = []
+var spliteven: Int
+
+
+
+//Define 
+
+pretaxtotal = 45.52
+posttaxtotal = 48.52
+tippercent = 0.20
+numsplit = 5
+spliteven = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
