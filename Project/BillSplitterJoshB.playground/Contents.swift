007f81f1dc9a82ec1870211d1a38d3e99a0ef2a7
Josh <joshb7100@gmail.com>
2016-11-08 14:23:27 -0600

    Finished bill switching functionality

--- Project/BillSplitterJoshB.playground/Contents.swift
+++ Project/BillSplitterJoshB.playground/Contents.swift
@@ -40,10 +40,7 @@ Could also add in just a random function for paying someone back such as gas for
 
 */
 //An array to store multiple bills in order to switch between them
-
-
-var billArray = [bill]()
-billArray
+var currBillindex: Int = -1
 
 enum billType{
     case restaurant
@@ -55,15 +52,14 @@ enum billType{
 
 class bill{
     var type: billType = .undefined
-    var final: Double = 0
+    var ftotal: Double = 0
     var numsplit: Int = 0
     var percent: [Double] = []
     var finsplit: [Double] = []
     var even: Int = 1
     var arrayNum = -1
     func switchBill(){
-        billArray[currBill.arrayNum] = currBill
-        currBill = billArray[self.arrayNum]
+        currBillindex = arrayNum
     }
     
 }
@@ -78,25 +74,24 @@ class restBill: bill{
         self.tip = 0
         super.init()
         self.type = .restaurant
-        self.final = 0
+        self.ftotal = 0
         self.numsplit = 0
         self.percent = []
         self.finsplit = []
         self.even = 1
         self.arrayNum = billArray.count
-        print(arrayNum)
         billArray.insert(self, atIndex: arrayNum)
     }
 }
 
-
+var billArray = [restBill]()
 
 var currBill: bill = restBill()
-
-currBill = restBill()
+currBill.switchBill()
 
 
 //Set up any necessary variables
+/*
 var pretaxtotal: Double
 var posttaxtotal: Double
 var tippercent: Double
@@ -105,36 +100,39 @@ var numsplit: Int
 var splitpercent: [Double] = []
 var finalsplit: [Double] = []
 var spliteven: Int
+*/
 
 
-//Define our inputs to be used.
+//Define our inputs to be used for Bill #1
+print("Create Bill #1, fill in inputs and generate output:")
+billArray[currBillindex].pretax = 45.31
+billArray[currBillindex].posttax = 48.52
+billArray[currBillindex].tip = 0.20
+billArray[currBillindex].numsplit = 5
+billArray[currBillindex].even = 0
+billArray[currBillindex].percent += [0.40,0.2,0.1,0.15,0.15]
 
-pretaxtotal = 45.31
-posttaxtotal = 48.52
-tippercent = 0.20
-numsplit = 5
-spliteven = 1
-splitpercent += [0.40,0.2,0.1,0.15,0.15]
-
-func restaurantSplit (pretax:Double, posttax:Double, tip:Double, split:Int, even: Int) -> Double{
+func restaurantSplit (billIndex: Int) -> Double{
+    //Clear the final split array of any pregenerated outputs.
+    billArray[billIndex].finsplit = []
     //Calculate the tip based on the pretax total.
-    let tipamount: Double = tip * pretax
+    let tipamount: Double = billArray[billIndex].tip * billArray[billIndex].pretax
     //Determine the final total based on tip + posttax total
-    var finaltot = tipamount + posttax
+    var finaltot = tipamount + billArray[billIndex].posttax
     //Round final total to two decimal places
     finaltot = twodecimal(finaltot)
     //Calculate out the amount each individual pays either by splitting evenly or taking in the inputted percents
-    if(even == 1){
-        for _ in 1...split{
+    if(billArray[billIndex].even == 1){
+        for _ in 1...billArray[billIndex].numsplit{
         //Append each result that is rounded to two decimals
-            finalsplit.append(twodecimal(finaltot / Double(split)))
+            billArray[billIndex].finsplit.append(twodecimal(finaltot / Double(billArray[billIndex].numsplit)))
         }
     }
     else{
-        for i in 1...split{
-            let splitamt = twodecimal(finaltot * splitpercent[i - 1])
+        for i in 1...billArray[billIndex].numsplit{
+            let splitamt = twodecimal(finaltot * billArray[billIndex].percent[i - 1])
             //print(splitamt)
-            finalsplit.append(splitamt)
+            billArray[billIndex].finsplit.append(splitamt)
         }
     }
     
@@ -157,22 +155,44 @@ func twodecimal (number:Double) -> Double{
 }
 
 //Output function to help with printing out the returns from the bill splitting.
-func output (splitamts: [Double], total: Double){
-    print("Including a \(tippercent * 100)% tip, the final total is $\(total)")
+func output (billIndex: Int){
+    print("Including a \(billArray[billIndex].tip * 100)% tip, the final total is $\(billArray[billIndex].ftotal)")
     print("The split amounts accordingly are:")
-    if(spliteven == 1){
-        print("Each person pays an even split of $\(splitamts[0])")
+    if(billArray[billIndex].even == 1){
+        print("Each of the \(billArray[billIndex].numsplit) people pays an even split of $\(billArray[billIndex].finsplit[0])")
     }
     else{
-        for i in 1...numsplit{
-         print("\(splitpercent[i - 1] * 100)% will be $\(splitamts[i - 1])")
+        for i in 1...billArray[billIndex].numsplit{
+         print("\(billArray[billIndex].percent[i - 1] * 100)% will be $\(billArray[billIndex].finsplit[i - 1])")
         }
     }
+    print(" ")
 }
 
 //Run our restaurantSplit function with our defined inputs
-finaltotal = restaurantSplit(pretaxtotal,posttax: posttaxtotal,tip: tippercent,split: numsplit, even: spliteven)
+billArray[currBillindex].ftotal = restaurantSplit(currBillindex)
 //Call our output function to print out the results.
-output(finalsplit, total: finaltotal)
+output(currBillindex)
+
+currBill = restBill()
+currBill.switchBill()
+
+//Bill #2 inputs
+print("Create Bill #2, switch to it, fill in inputs and output results:")
+billArray[currBillindex].pretax = 158.32
+billArray[currBillindex].posttax = 183.21
+billArray[currBillindex].tip = 0.15
+billArray[currBillindex].numsplit = 8
+billArray[currBillindex].even = 0
+billArray[currBillindex].percent += [0.1,0.2,0.1,0.05,0.05,0.1,0.1,0.3]
+billArray[currBillindex].ftotal = restaurantSplit(currBillindex)
+output(currBillindex)
+
+//Switch back to bill 1
+print("Switch back to Bill #1, change input and regenerate outputs:")
+billArray[0].switchBill()
+billArray[currBillindex].tip = 0.1
+billArray[currBillindex].ftotal = restaurantSplit(currBillindex)
+output(currBillindex)
 
 
